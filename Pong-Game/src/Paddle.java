26631e91c67dc1e6e8ebff9832791b1c433de1f2
Maciej Wężyk <maciek.wezyk@gmail.com>
2016-09-28 11:33:28 +0200

    Add files via upload
    Decided to write that game again - want to implement some extras, but for now it is working well

---
+++ Pong-Game/src/Paddle.java
@@ -0,0 +1,52 @@
+import java.awt.*;
+
+
+public class Paddle{
+    public int paddleNumber;
+
+    public int x, y, width = 30, height = 175;
+
+    public int score;
+
+    public Color c;
+
+    public Paddle(Pong pong, int paddleNumber) {
+        this.paddleNumber = paddleNumber;
+
+        if (paddleNumber == 1) {
+            this.x = 0;
+        }
+        if (paddleNumber == 2) {
+            this.x = pong.getWidth() - width - 1;
+        }
+        this.y = pong.getHeight()/2 - this.height/2;
+    }
+
+    public void render(Graphics2D g) {
+        if (score < Pong.pong.scoreLimit - 2) {
+            g.setColor(Color.CYAN);
+        } else {
+            c = Color.MAGENTA;
+            g.setColor(c);
+            g.fillRect(x, y, width, height);
+        }
+        g.drawRect(x, y, width, height);
+    }
+
+    public void move(boolean up) {
+        int speed = 15;
+        if(up) {
+            if (y - speed > 0) {
+                y -= speed;
+            } else {
+                y = 0;
+            }
+        } else {
+            if (y + height + speed < Pong.pong.getHeight()) {
+                y+= speed;
+            } else {
+                y = Pong.pong.getHeight() - height -1;
+            }
+        }
+    }
+}
