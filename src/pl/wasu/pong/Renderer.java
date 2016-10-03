04ca4ff1b07c5b1c357eb599097cb48ae5ef9647
wasu77 <maciek.wezyk@gmail.com>
2016-10-03 14:18:05 +0200

    last version

---
+++ src/pl/wasu/pong/Renderer.java
@@ -0,0 +1,17 @@
+package pl.wasu.pong;
+
+import pl.wasu.pong.Pong;
+
+import javax.swing.*;
+
+
+import java.awt.*;
+
+public class Renderer extends JPanel {
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Pong.pong.render((Graphics2D) g);
+
+    }
+}
